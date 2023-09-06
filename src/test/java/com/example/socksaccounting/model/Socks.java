bc4a81c58541b10db5913fc97c5c5f42fdaa9830
KrozhDev <roko.mdn@gmail.com>
2023-09-06 20:11:50 +0300

    model added

--- src/test/java/com/example/socksaccounting/model/Socks.java
+++ src/test/java/com/example/socksaccounting/model/Socks.java
@@ -1,4 +1,28 @@
 package com.example.socksaccounting.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Socks {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NotNull
+    private String color;
+    @Min(1)
+    @Max(100)
+    private Integer cottonPart;
+    @Min(1)
+    private Integer quantity;
 }
